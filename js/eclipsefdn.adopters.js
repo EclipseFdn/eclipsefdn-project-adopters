d93c7b9c77c8aacb4c9c5f215ab6cc818f31bfd4
Martin Lowe <martin.lowe@eclipse-foundation.org>
2020-12-04 16:21:39 -0500

    Add button with project ID to heading of project sections
    Signed-off-by: Martin Lowe <martin.lowe@eclipse-foundation.org>

--- js/eclipsefdn.adopters.js
+++ js/eclipsefdn.adopters.js
@@ -141,6 +141,12 @@
       for (var i = 0; i < el.length; i++) {
         el[i].append(h2);
       }
+      const headerAnchor = document.createElement('a');
+      headerAnchor.setAttribute('class', 'btn btn-xs btn-secondary margin-left-10');
+      headerAnchor.setAttribute('href', 'https://projects.eclipse.org/projects/' + project.project_id);
+      headerAnchor.textContent = project.project_id;
+      h2.appendChild(headerAnchor);
+
       createProjectList(json_object, projectOpts, el);
     }
   }
