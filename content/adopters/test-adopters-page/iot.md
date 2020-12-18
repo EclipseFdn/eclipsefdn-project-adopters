fd7598f0e34254437f55212b55677c6e3a224ef7
Yi Liu <yi.liu@eclipse-foundation.org>
2020-12-18 11:06:00 -0500

    improvements after comments: change to use var, use variable to define baseURL
    Signed-off-by: Yi Liu <yi.liu@eclipse-foundation.org>

--- content/adopters/test-adopters-page/iot.md
+++
@@ -1,9 +0,0 @@
----
-title: "IoT Project Adopters"
-date: 2019-09-10T15:50:25-04:00
-# working_group: internet-things-iot
----
-
-This is only for preview on netlify
-
-{{< page-adopters-test >}}
--- js/adopters_test.js
+++ js/adopters_test.js
@@ -1,7 +1,7 @@
 const eclipsefdn_adopters_element = document.querySelectorAll(".eclipsefdn-adopters")[0];
 
-function testAdopters(working_group) {
-	fetch("/adopters/assets/js/adopters.json")
+function testAdopters(baseURL) {
+	fetch(`${baseURL}/assets/js/adopters.json`)
 	.then(response => {
 		if (response.ok) {
 			return response.json();
@@ -9,18 +9,18 @@ function testAdopters(working_group) {
 			return Promise.reject(response);
 		}
 	})
-	.then(data => createWGAdopters(data.adopters, working_group))
+	.then(data => createWGAdopters(data.adopters, baseURL))
 	.catch(error => console.log(error))
 }
 
-function createWGAdopters(data, working_group) {
+function createWGAdopters(data, baseURL) {
 
-    let project_set = new Set();
-    for(let i=0; i<data.length; i++) {
+    var project_set = new Set();
+    for(var i=0; i<data.length; i++) {
 
         // Loop the projects of this adopter
-        for(let j=0; j<data[i].projects.length; j++) {
-			let project = data[i].projects[j];
+        for(var j=0; j<data[i].projects.length; j++) {
+			var project = data[i].projects[j];
 
             // If not exists this project
             if( !project_set.has(project) ) {
@@ -28,30 +28,30 @@ function createWGAdopters(data, working_group) {
                 // add the title
 				createProjectHeader(project);
 			}
-			createAdopters(project, data[i]);
+			createAdopters(project, data[i], baseURL);
 		}
 	}
 }
 
 function createProjectHeader(project) {
-	const h2 = document.createElement('h2');
+	var h2 = document.createElement('h2');
 	h2.textContent = project;
 	h2.setAttribute("id", project);
 	eclipsefdn_adopters_element.append(h2);
 	// add the button
-	const headerAnchor = document.createElement('a');
+	var headerAnchor = document.createElement('a');
 	headerAnchor.setAttribute('class', 'btn btn-xs btn-secondary margin-left-10');
 	headerAnchor.setAttribute('href', 'https://projects.eclipse.org/projects/' + project);
 	headerAnchor.textContent = project;
 	h2.appendChild(headerAnchor);
-	const ul = document.createElement('ul');
+	var ul = document.createElement('ul');
 	ul.setAttribute("class", "text-center list-inline");
 	ul.setAttribute("id", `${project}-ul`);
 	eclipsefdn_adopters_element.append(ul);
 }
 
-function createAdopters(project, adopter) {
-	let element = document.getElementById(`${project}-ul`);
+function createAdopters(project, adopter, baseURL) {
+	var element = document.getElementById(`${project}-ul`);
 
 	// Get the home page url of this adopter
 	var url = '';
@@ -69,12 +69,12 @@ function createAdopters(project, adopter) {
 		logo = adopter['logo'];
 	}
 	// Create the html elements
-	let li = document.createElement('li');
-	let a = document.createElement('a');
-	let img = document.createElement('img');
+	var li = document.createElement('li');
+	var a = document.createElement('a');
+	var img = document.createElement('img');
 	a.setAttribute('href', url);
 	img.setAttribute('alt', name);
-	img.setAttribute('src', '/adopters/assets/images/adopters/' + logo);
+	img.setAttribute('src', baseURL + '/assets/images/adopters/' + logo);
 	img.setAttribute('class', 'adopters-img');
 	a.appendChild(img);
 	li.appendChild(a);
