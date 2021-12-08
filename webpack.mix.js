427cfec95c5ab84d2a40268b59abeb8eb92b620d
Zhou Fang <zhou.fang@eclipse-foundation.org>
2021-12-08 12:30:09 -0500

    Updated hugo-theme and webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,33 +11,14 @@
  * SPDX-License-Identifier: EPL-2.0
 */
 
+require('./node_modules/eclipsefdn-solstice-assets/webpack-solstice-assets.mix.js');
 let mix = require('laravel-mix');
+mix.EclipseFdnSolsticeAssets();
+
 mix.setPublicPath('static/assets');
 mix.setResourceRoot('../');
 
 // CSS
 mix.less('./less/styles.less', './static/assets/css/styles.css');
 
-// JavaScript
-mix.scripts([
-    './node_modules/jquery/dist/jquery.min.js',
-    './node_modules/bootstrap/dist/js/bootstrap.min.js',
-    './node_modules/jquery-match-height/dist/jquery.matchHeight-min.js',
-    './node_modules/jquery-eclipsefdn-api/dist/jquery.eclipsefdn-api.min.js',
-    './node_modules/feather-icons/dist/feather.min.js',
-    './node_modules/cookieconsent/src/cookieconsent.js',
-    './node_modules/eclipsefdn-solstice-assets/js/solstice.cookieconsent.js',
-    './node_modules/eclipsefdn-solstice-assets/js/eclipsefdn.videos.js',
-    './node_modules/eclipsefdn-solstice-assets/js/solstice.cookies.js',
-    './node_modules/eclipsefdn-solstice-assets/js/solstice.js'
-], './static/assets/js/scripts.min.js');
-mix.babel([
-    './node_modules/eclipsefdn-solstice-assets/js/eclipsefdn.adopters.js',
-], './static/assets/js/eclipsefdn.adopters.js');
-
-//For IE compatible with fetch
-mix.scripts([
-    './node_modules/promise-polyfill/dist/polyfill.min.js',
-    './node_modules/unfetch/polyfill/index.js',
-    './js/adopters_test.js',
-], './static/assets/js/adopters_test.js');
+mix.js('js/main.js', './static/assets/js/scripts.min.js');
